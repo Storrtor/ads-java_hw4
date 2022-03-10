15c3ee57c73a53e7dc7ac39fc360197e153d44cb
stortor <storrtor@gmail.com>
2022-03-10 17:05:07 +0300

    done

--- src/main/java/lesson4/deque/Deque.java
+++ src/main/java/lesson4/deque/Deque.java
@@ -2,7 +2,9 @@ package lesson4.deque;
 
 
 import lesson3.queue.Queue;
+import lesson4.LinkedList;
 
+//1. Реализовать Deque (с возможностью обратного прохода) 1:18:00 - deque 1:27:00 - спойлер к декью
 public interface Deque<E> extends Queue<E> {
 
     boolean insertLeft(E value);
@@ -10,4 +12,16 @@ public interface Deque<E> extends Queue<E> {
 
     E removeLeft();
     E removeRight();
+
+    class NodeDeq<E> {
+        E item;
+        NodeDeq<E> next;
+        NodeDeq<E> prev;
+
+        public NodeDeq(E item, NodeDeq<E> next, NodeDeq<E> prev) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
 }
