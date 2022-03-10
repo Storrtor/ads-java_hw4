7e4f37a248e16053d53a17d4bcedc60c611c8a08
stortor <storrtor@gmail.com>
2022-03-10 17:09:08 +0300

    done

--- src/main/java/lesson4/deque/Deque.java
+++ src/main/java/lesson4/deque/Deque.java
@@ -4,7 +4,7 @@ package lesson4.deque;
 import lesson3.queue.Queue;
 import lesson4.LinkedList;
 
-//1. Реализовать Deque (с возможностью обратного прохода) 1:18:00 - deque 1:27:00 - спойлер к декью
+//1. Реализовать Deque
 public interface Deque<E> extends Queue<E> {
 
     boolean insertLeft(E value);
